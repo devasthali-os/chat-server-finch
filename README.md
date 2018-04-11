42d4206c86f5d03dc60b1a708205cbac5fe41cfe
Upadhyay <prayag.upd@gmail.com>
2018-04-11 00:47:18 -0700

    add get with http -header

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 
-
+```
+sbt run
 ```
 
+```
+curl -H "correlationID: 12345678" localhost:9090/init
+{"correlationID":"94da2874-956e-4f3a-acb6-850df37adbb9","message":"Hi, How can I help you?"}
+```
+
+```
 curl -d '{"correlationID": "9c327ed6-05ad-4df6-beab-875c33906aab", "message": "can i know about renters insurance"}' localhost:9090/chat
-{"correlationID":"9c327ed6-05ad-4df6-beab-875c33906aab","displayText":"Hi, How can I help you?"}
+{"correlationID":"9c327ed6-05ad-4df6-beab-875c33906aab","displayText":"Here are some coffee shops:"}
 ```
--- src/main/scala/com/chat/server/ChatServer.scala
+++ src/main/scala/com/chat/server/ChatServer.scala
@@ -1,12 +1,14 @@
 package com.chat.server
 
+import java.util.UUID
+
 import com.twitter.finagle.Http
 import com.twitter.util.Await
-
 import io.finch._
 import io.finch.circe._
 import io.finch.syntax._
 import io.circe.generic.auto._
+import com.twitter.util.Future
 
 object ChatServer {
 
@@ -15,13 +17,29 @@ object ChatServer {
     case class ChatRequest(correlationID: String, message: String)
     case class ChatResponse(correlationID: String, displayText: String)
 
+    trait InitResponse
+    case class ChatInitResponse(correlationID: String, message: String) extends InitResponse
+    case class InvalidResponse(error: String) extends InitResponse
+
+    val chatInitHeader: Endpoint[String] =
+      header("correlationID").mapOutputAsync(id => {
+        if (id == null) Future.value(Ok("Invalid correlationID"))
+        else Future.value(Ok(UUID.randomUUID().toString))
+      })
+
+    val chatInit: Endpoint[ChatInitResponse] = get("init" :: chatInitHeader) { id: String =>
+      Ok(ChatInitResponse(id, "Hi, How can I help you?"))
+    }
+
     val chat: Endpoint[ChatResponse] =
       post("chat" :: jsonBody[ChatRequest]) { chatRequest: ChatRequest =>
 
-        Ok(ChatResponse(chatRequest.correlationID, "Hi, How can I help you?"))
+        Ok(ChatResponse(chatRequest.correlationID, "Here are some coffee shops"))
       }
 
-    Await.ready(Http.server.serve(":9090", chat.toService))
+    val endpoints = (chatInit :+: chat).toService
+
+    Await.ready(Http.server.serve(":9090", endpoints))
 
   }
 }
